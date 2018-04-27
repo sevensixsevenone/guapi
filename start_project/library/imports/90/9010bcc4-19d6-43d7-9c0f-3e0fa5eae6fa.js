3db2435a14609d0082397ea3fa6993ad03141d7b
zk571 <719129139@qq.com>
2018-04-27 09:09:08 +0800

    1111

---
+++ start_project/library/imports/90/9010bcc4-19d6-43d7-9c0f-3e0fa5eae6fa.js
@@ -0,0 +1,86 @@
+"use strict";
+cc._RF.push(module, '9010bzEGdZD15wPPg+l6ub6', 'Star');
+// scripts/Star.js
+
+"use strict";
+
+var _cc$Class;
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class((_cc$Class = {
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        pickRadius: 0,
+        // 暂存 Game 对象的引用
+        game: {
+            default: null,
+            serializable: false
+            //     default: null,        // The default value will be used only when the component attaching
+            //                           // to a node for the first time
+            //     type: cc.SpriteFrame, // optional, default is typeof default
+            //     serializable: true,   // optional, default is true
+            // },
+            // bar: {
+            //     get () {
+            //         return this._bar;
+            //     },
+            //     set (value) {
+            //         this._bar = value;
+            //     }
+            // },
+        } },
+    onLoad: function onLoad() {},
+
+    getPlayerDistance: function getPlayerDistance() {
+        // 根据 player 节点位置判断距离
+        var playerPos = this.game.player.getPosition();
+        // 根据两点位置计算两点之间距离
+        var dist = cc.pDistance(this.node.position, playerPos);
+        return dist;
+    },
+
+    onPicked: function onPicked() {
+        // 当星星被收集时，调用 Game 脚本中的接口，生成一个新的星星
+        this.game.spawnNewStar();
+        // 然后销毁当前星星节点
+        this.node.destroy();
+    },
+    update: function update(dt) {
+        // 每帧判断和主角之间的距离是否小于收集距离
+        if (this.getPlayerDistance() < this.pickRadius) {
+            // 调用收集行为
+            this.onPicked();
+            return;
+        }
+    }
+
+}, _defineProperty(_cc$Class, "onPicked", function onPicked() {
+    // 当星星被收集时，调用 Game 脚本中的接口，生成一个新的星星
+    this.game.spawnNewStar();
+    // 调用 Game 脚本的得分方法
+    this.game.gainScore();
+    // 然后销毁当前星星节点
+    this.node.destroy();
+}), _defineProperty(_cc$Class, "update", function update() {
+    // ...
+    // 根据 Game 脚本中的计时器更新星星的透明度
+    var opacityRatio = 1 - this.game.timer / this.game.starDuration;
+    var minOpacity = 50;
+    this.node.opacity = minOpacity + Math.floor(opacityRatio * (255 - minOpacity));
+}), _defineProperty(_cc$Class, "start", function start() {}), _cc$Class));
+
+cc._RF.pop();
